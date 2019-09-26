3f5651a0f46e6531f37841e0fcda10eae5da850e
Anatoly <east@thyloved.ru>
2019-09-26 22:03:38 +0300

    Initial commit

---
+++ nifi-compose-processors/src/main/java/com/compose/nifi/processors/MongoWrapper.java
@@ -0,0 +1,303 @@
+package com.compose.nifi.processors;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoClientURI;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.WriteConcern;
+import com.mongodb.client.MongoDatabase;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.nifi.authorization.exception.AuthorizerCreationException;
+import org.apache.nifi.components.PropertyDescriptor;
+import org.apache.nifi.components.AllowableValue;
+
+import org.apache.nifi.processor.ProcessContext;
+import org.apache.nifi.processor.util.StandardValidators;
+import org.apache.nifi.components.Validator;
+import org.apache.nifi.security.util.SslContextFactory;
+import org.apache.nifi.ssl.SSLContextService;
+
+import javax.net.ssl.SSLContext;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by hayshutton on 8/6/16.
+ */
+class MongoWrapper {
+  private static final Logger logger = LoggerFactory.getLogger(MongoWrapper.class);
+  static final String WRITE_CONCERN_ACKNOWLEDGED = "ACKNOWLEDGED";
+  static final String WRITE_CONCERN_UNACKNOWLEDGED = "UNACKNOWLEDGED";
+  static final String WRITE_CONCERN_JOURNALED = "JOURNALED";
+  static final String WRITE_CONCERN_MAJORITY = "MAJORITY";
+  static final String W1 = "W1";
+  static final String W2 = "W2";
+  static final String W3 = "W3";
+  static final String SEPARATED = "Separate mongodb params";
+  static final String SINGLE = "Single connection string";
+
+  private static final PropertyDescriptor URI = new PropertyDescriptor.Builder()
+          .name("Mongo conection string (URI)")
+          .description("MongoURI, typically of the form: mongodb://host1[:port1][,host2[:port2],...]")
+          .required(false)
+          .addValidator(Validator.VALID)
+          .build();
+  private static final PropertyDescriptor DATABASE_NAME = new PropertyDescriptor.Builder()
+          .name("Mongo Database Name")
+          .description("The name of the database to use")
+          .required(true)
+          .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+          .build();
+  public static final AllowableValue CONNECTION_STRING = new AllowableValue(SINGLE, "Single connection string",
+          "Mongo connection string (URI) will be used to establish connection");
+  public static final AllowableValue SEPARATED_PARAMS = new AllowableValue(SEPARATED, "Separate mongodb params",
+          "Separated fields with mongo params values will be used to establish connection");
+  public static final PropertyDescriptor CONNECTION_SOURCE = new PropertyDescriptor.Builder()
+          .name("Connection string / separate mongo params")
+          .description("Separated params or single connection string to be used during establishing connection")
+          .allowableValues(CONNECTION_STRING, SEPARATED_PARAMS)
+          .defaultValue(CONNECTION_STRING.getValue())
+          .addValidator(Validator.VALID)
+          .build();
+  private static final PropertyDescriptor MONGO_HOST = new PropertyDescriptor.Builder()
+          .name("Mongo host")
+          .description("Mongo host. Use either URI, either host value of the form: host:port")
+          .required(false)
+          .addValidator(Validator.VALID)
+          .build();
+  private static final PropertyDescriptor AUTH_SOURCE = new PropertyDescriptor.Builder()
+          .name("Mongo Authentication Source")
+          .description("The name of the database in which the user is defined. If field is left blank the database name to capture changes will be used as authentication source")
+          .required(false)
+          .addValidator(Validator.VALID)
+          .build();
+  private static final PropertyDescriptor USERNAME = new PropertyDescriptor.Builder()
+          .name("Mongo Username")
+          .description("Mongo username for authentication")
+          .required(false)
+          .sensitive(true)
+          .addValidator(Validator.VALID)
+          .build();
+  private static final PropertyDescriptor PASSWORD = new PropertyDescriptor.Builder()
+          .name("Mongo Password")
+          .description("Mongo user's password for authentication")
+          .required(false)
+          .sensitive(true)
+          .addValidator(Validator.VALID)
+          .build();
+  private static final PropertyDescriptor WHITE_LIST_COLLECTION_NAMES = new PropertyDescriptor.Builder()
+          .name("Mongo White List Collections Names")
+          .description("The name of white list collections of the database to use")
+          .required(false)
+          .addValidator(Validator.VALID)
+          .build();
+  private static final PropertyDescriptor SSL_CONTEXT_SERVICE = new PropertyDescriptor.Builder()
+          .name("ssl-context-service")
+          .displayName("SSL Context Service")
+          .description("The SSL Context Service used to provide client certificate information for TLS/SSL "
+                  + "connections.")
+          .required(false)
+          .identifiesControllerService(SSLContextService.class)
+          .build();
+  private static final PropertyDescriptor CLIENT_AUTH = new PropertyDescriptor.Builder()
+          .name("ssl-client-auth")
+          .displayName("Client Auth")
+          .description("Client authentication policy when connecting to secure (TLS/SSL) cluster. "
+                  + "Possible values are REQUIRED, WANT, NONE. This property is only used when an SSL Context "
+                  + "has been defined and enabled.")
+          .required(false)
+          .allowableValues(SSLContextService.ClientAuth.values())
+          .defaultValue("REQUIRED")
+          .build();
+
+  public static final PropertyDescriptor WRITE_CONCERN = new PropertyDescriptor.Builder()
+        .name("Write Concern")
+        .description("The write concern to use")
+        .required(true)
+        .allowableValues(WRITE_CONCERN_ACKNOWLEDGED, WRITE_CONCERN_UNACKNOWLEDGED,
+                WRITE_CONCERN_JOURNALED, WRITE_CONCERN_MAJORITY,
+                W1, W2, W3)
+        .defaultValue(WRITE_CONCERN_ACKNOWLEDGED)
+        .build();
+
+  private final static String SYSTEM_INDEXES = "system.indexes";
+  static final Pattern systemIndexesPattern = Pattern.compile(SYSTEM_INDEXES);
+
+
+  static List<PropertyDescriptor> descriptors = new ArrayList<>();
+
+  static {
+    descriptors.add(URI);
+    descriptors.add(DATABASE_NAME);
+    descriptors.add(CONNECTION_SOURCE);
+    descriptors.add(MONGO_HOST);
+    descriptors.add(AUTH_SOURCE);
+    descriptors.add(USERNAME);
+    descriptors.add(PASSWORD);
+    descriptors.add(WHITE_LIST_COLLECTION_NAMES);
+    descriptors.add(SSL_CONTEXT_SERVICE);
+    descriptors.add(CLIENT_AUTH);
+  }
+
+  private MongoClient mongoClient;
+
+  private MongoClientOptions.Builder getClientOptions(final SSLContext sslContext) {
+    MongoClientOptions.Builder builder = MongoClientOptions.builder();
+    builder.sslEnabled(true);
+    builder.socketFactory(sslContext.getSocketFactory());
+    return builder;
+  }
+
+  public String getConnectionSource(final ProcessContext context) {
+    return context.getProperty(CONNECTION_SOURCE).getValue();
+  }
+
+  public String getMongoHost(final ProcessContext context) {
+    return context.getProperty(MONGO_HOST).getValue();
+  }
+
+  public String getDatabaseName(final ProcessContext context) {
+    return context.getProperty(DATABASE_NAME).getValue();
+  }
+
+  public MongoDatabase getDatabase(final ProcessContext context) {
+    final String databaseName = context.getProperty(DATABASE_NAME).getValue();
+    return mongoClient.getDatabase(databaseName);
+  }
+
+  public MongoDatabase getDatabase(final String databaseName) {
+    return mongoClient.getDatabase(databaseName);
+  }
+
+  public MongoDatabase getLocalDatabase() {
+    return mongoClient.getDatabase("local");
+  }
+
+  public String getUsername(final ProcessContext context) {
+    return context.getProperty(USERNAME).getValue();
+  }
+
+  public char[] getPassword(final ProcessContext context) {
+    return context.getProperty(PASSWORD).getValue().toCharArray();
+  }
+
+  public String getAuthSource(final ProcessContext context) {
+    return context.getProperty(AUTH_SOURCE).getValue();
+  }
+
+  public String getURI(final ProcessContext context) {
+    return context.getProperty(URI).getValue();
+  }
+
+  public ArrayList getWhiteListCollectionNames(final ProcessContext context) {
+    String whiteListCollectionNames = context.getProperty(WHITE_LIST_COLLECTION_NAMES).getValue().trim();
+    if (StringUtils.isBlank(whiteListCollectionNames)) {
+      return null;
+    }
+    return new ArrayList<String>(Arrays.asList(whiteListCollectionNames.split("\\s*,\\s*")));
+  }
+
+  public void createClient(ProcessContext context) throws IOException {
+    if (mongoClient != null) {
+      closeClient();
+    }
+
+    final SSLContextService sslService = context.getProperty(SSL_CONTEXT_SERVICE).asControllerService(SSLContextService.class);
+    final String rawClientAuth = context.getProperty(CLIENT_AUTH).getValue();
+    final SSLContext sslContext;
+
+    if (sslService != null) {
+      final SSLContextService.ClientAuth clientAuth;
+      if (StringUtils.isBlank(rawClientAuth)) {
+        clientAuth = SSLContextService.ClientAuth.REQUIRED;
+      } else {
+        try {
+          clientAuth = SSLContextService.ClientAuth.valueOf(rawClientAuth);
+        } catch (final IllegalArgumentException iae) {
+          throw new AuthorizerCreationException(String.format("Unrecognized client auth '%s'. Possible values are [%s]",
+                  rawClientAuth, StringUtils.join(SslContextFactory.ClientAuth.values(), ", ")));
+        }
+      }
+      sslContext = sslService.createSSLContext(clientAuth);
+    } else {
+      sslContext = null;
+    }
+
+    try {
+      final String uri = this.getURI(context);
+      final String connectionSource = this.getConnectionSource(context);
+
+      if(sslContext == null) {
+        if (connectionSource.equals(SEPARATED)) {
+          final String databaseName = this.getDatabaseName(context);
+          String database = databaseName;
+          
+          final String username = this.getUsername(context);
+          final char[] password = this.getPassword(context);
+          final String authSource = this.getAuthSource(context);
+          
+          if (authSource != null){
+            database = authSource;
+          }
+          String[] mongoServer = this.getMongoHost(context).trim().split(":");
+          MongoCredential credential = MongoCredential.createCredential(username, database, password);
+          mongoClient = new MongoClient(new ServerAddress(mongoServer[0], 
+                                                          Integer.parseInt(mongoServer[1])), 
+                                        Arrays.asList(credential));
+        } else {
+          mongoClient = new MongoClient(new MongoClientURI(uri));
+        }
+      } else {
+        mongoClient = new MongoClient(new MongoClientURI(uri, getClientOptions(sslContext)));
+      }
+    } catch (Exception e) {
+      throw e;
+    }
+  }
+
+  public final void closeClient() {
+    if (mongoClient != null) {
+      mongoClient.close();
+      mongoClient = null;
+    }
+  }
+
+  protected WriteConcern getWriteConcern(final ProcessContext context) {
+    final String writeConcernProperty = context.getProperty(WRITE_CONCERN).getValue();
+    WriteConcern writeConcern = null;
+    switch (writeConcernProperty) {
+      case WRITE_CONCERN_ACKNOWLEDGED:
+        writeConcern = WriteConcern.ACKNOWLEDGED;
+        break;
+      case WRITE_CONCERN_UNACKNOWLEDGED:
+        writeConcern = WriteConcern.UNACKNOWLEDGED;
+        break;
+      case WRITE_CONCERN_JOURNALED:
+        writeConcern = WriteConcern.JOURNALED;
+        break;
+      case WRITE_CONCERN_MAJORITY:
+        writeConcern = WriteConcern.MAJORITY;
+        break;
+      case W1:
+        writeConcern = WriteConcern.W1;
+        break;
+      case W2:
+        writeConcern = WriteConcern.W2;
+        break;
+      case W3:
+        writeConcern = WriteConcern.W3;
+        break;
+      default:
+        writeConcern = WriteConcern.ACKNOWLEDGED;
+    }
+    return writeConcern;
+  }
+
+}
---
+++ nifi-compose-processors/src/main/java/com/nifi/processors/ComposeTailingGetMongo.java
@@ -0,0 +1,189 @@
+package com.nifi.processors;
+
+
+import com.mongodb.CursorType;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoIterable;
+import org.apache.commons.io.IOUtils;
+import org.apache.nifi.annotation.behavior.InputRequirement;
+import org.apache.nifi.annotation.behavior.TriggerSerially;
+import org.apache.nifi.annotation.behavior.WritesAttribute;
+import org.apache.nifi.annotation.behavior.WritesAttributes;
+import org.apache.nifi.annotation.documentation.CapabilityDescription;
+import org.apache.nifi.annotation.documentation.Tags;
+import org.apache.nifi.annotation.lifecycle.OnScheduled;
+import org.apache.nifi.annotation.lifecycle.OnStopped;
+import org.apache.nifi.components.PropertyDescriptor;
+import org.apache.nifi.flowfile.FlowFile;
+import org.apache.nifi.processor.*;
+import org.apache.nifi.processor.io.OutputStreamCallback;
+import org.bson.BsonTimestamp;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.*;
+import java.util.regex.Pattern;
+
+import static com.mongodb.client.model.Filters.*;
+
+/**
+ * Created by hayshutton on 8/25/16.
+ */
+@TriggerSerially
+@InputRequirement(InputRequirement.Requirement.INPUT_FORBIDDEN)
+@Tags({"oplog", "mongodb", "get", "tailing"})
+@WritesAttributes({
+          @WritesAttribute(attribute = "mime.type", description = "This is the content-type for the content."),
+          @WritesAttribute(attribute = "mongo.id", description = "The MongoDB object_id for the document in hex format or the 'h' from the oplog document."),
+          @WritesAttribute(attribute = "mongo.ts", description = "Timestamp of operation from oplog or timestamp of query prior to tailing."),
+          @WritesAttribute(attribute = "mongo.op", description = "The Mongo operation. `i' for insert, 'd' for delete, 'u' for update, 'q' which is a placeholder for query result when not an oplog operation"),
+          @WritesAttribute(attribute = "mongo.db", description = "The Mongo database name"),
+          @WritesAttribute(attribute = "mongo.collection", description = "The Mongo collection name")
+})
+@CapabilityDescription("Dumps documents from a MongoDB and then dumps operations from the oplog in soft real time. The FlowFile content is the document itself from the find or the `o` attribute from the oplog. It keeps a connection open and waits on new oplog entries. Restart does the full dump again and then oplog tailing.")
+public class ComposeTailingGetMongo extends AbstractSessionFactoryProcessor {
+  private static final Relationship REL_SUCCESS = new Relationship.Builder().name("success").description("the happy path for mongo documents and operations").build();
+
+  private static final Set<Relationship> relationships;
+
+  private static final List<PropertyDescriptor> propertyDescriptors;
+
+  static {
+    List<PropertyDescriptor> _propertyDescriptors = new ArrayList<>();
+    _propertyDescriptors.addAll(MongoWrapper.descriptors);
+    propertyDescriptors = Collections.unmodifiableList(_propertyDescriptors);
+
+    Set<Relationship> _relationships = new HashSet<>();
+    _relationships.add(REL_SUCCESS);
+    relationships = Collections.unmodifiableSet(_relationships);
+  }
+
+  private MongoWrapper mongoWrapper;
+
+  @Override
+  public final Set<Relationship> getRelationships() {
+    return relationships;
+  }
+
+  @Override
+  public final List<PropertyDescriptor> getSupportedPropertyDescriptors() {
+    return propertyDescriptors;
+  }
+
+  @OnScheduled
+  public final void createClient(ProcessContext context) throws IOException {
+    mongoWrapper = new MongoWrapper();
+    mongoWrapper.createClient(context);
+  }
+
+  @OnStopped
+  public final void closeClient() {
+    mongoWrapper.closeClient();
+  }
+
+  @Override
+  public final void onTrigger(final ProcessContext context, final ProcessSessionFactory sessionFactory) {
+    long ts = new Date().getTime();
+    BsonTimestamp bts = new BsonTimestamp((int) (new Date().getTime() / 1000), 0);
+    String dbName = mongoWrapper.getDatabase(context).getName();
+    MongoIterable<String> collectionNames = mongoWrapper.getDatabase(context).listCollectionNames();
+    for(String collectionName: collectionNames) {
+      if(MongoWrapper.systemIndexesPattern.matcher(collectionName).matches()) {
+        continue;
+      }
+      MongoCollection<Document> collection = mongoWrapper.getDatabase(context).getCollection(collectionName);
+
+      try {
+        FindIterable<Document> it = collection.find();
+        MongoCursor<Document> cursor = it.iterator();
+
+        try {
+          while(cursor.hasNext()) {
+            ProcessSession session = sessionFactory.createSession();
+            FlowFile flowFile = session.create();
+            Document currentDoc = cursor.next();
+            //TODO when not object_id
+            ObjectId currentObjectId = currentDoc.getObjectId("_id");
+            flowFile = session.putAttribute(flowFile, "mime.type", "application/json");
+            flowFile = session.putAttribute(flowFile, "mongo.id", currentObjectId.toHexString());
+            flowFile = session.putAttribute(flowFile, "mongo.ts", bts.toString());
+            flowFile = session.putAttribute(flowFile, "mongo.op", "q");
+            flowFile = session.putAttribute(flowFile, "mongo.db", dbName);
+            flowFile = session.putAttribute(flowFile, "mongo.collection", collectionName);
+
+            flowFile = session.write(flowFile, new OutputStreamCallback() {
+              @Override
+              public void process(OutputStream outputStream) throws IOException {
+                IOUtils.write(currentDoc.toJson(), outputStream);
+              }
+            });
+            session.getProvenanceReporter().receive(flowFile, mongoWrapper.getURI(context));
+            session.transfer(flowFile, REL_SUCCESS);
+            session.commit();
+          }
+        } finally {
+          cursor.close();
+        }
+      } catch (Throwable t) {
+        getLogger().error("{} failed to process due to {}; rolling back", new Object[]{this, t});
+        throw t;
+      }
+    }
+
+    MongoCollection<Document> oplog = mongoWrapper.getLocalDatabase().getCollection("oplog.rs");
+    try {
+      FindIterable<Document> it = oplog.find(gt("ts", bts)).cursorType(CursorType.TailableAwait).oplogReplay(true).noCursorTimeout(true);
+      MongoCursor<Document> cursor = it.iterator();
+      try {
+        while(cursor.hasNext()){
+          ProcessSession session = sessionFactory.createSession();
+          Document currentDoc = cursor.next();
+          String[] namespace = currentDoc.getString("ns").split(Pattern.quote("."));
+          if(dbName.equals(namespace[0])) {
+            FlowFile flowFile = session.create();
+            Document oDoc = currentDoc.get("o", Document.class);
+            String h = Long.toString(currentDoc.getLong("h"));
+            flowFile = session.putAttribute(flowFile, "mime.type", "application/json");
+            flowFile = session.putAttribute(flowFile, "mongo.id", getId(currentDoc));
+            flowFile = session.putAttribute(flowFile, "mongo.ts", currentDoc.get("ts", BsonTimestamp.class).toString());
+            flowFile = session.putAttribute(flowFile, "mongo.op", currentDoc.getString("op"));
+            flowFile = session.putAttribute(flowFile, "mongo.db", dbName);
+            flowFile = session.putAttribute(flowFile, "mongo.collection", namespace[1]);
+
+            flowFile = session.write(flowFile, new OutputStreamCallback() {
+              @Override
+              public void process(OutputStream outputStream) throws IOException {
+                IOUtils.write(oDoc.toJson(), outputStream);
+              }
+            });
+            session.getProvenanceReporter().receive(flowFile, mongoWrapper.getURI(context));
+            session.transfer(flowFile, REL_SUCCESS);
+            session.commit();
+          }
+        }
+      } finally {
+        cursor.close();
+      }
+    } catch (Throwable t) {
+      getLogger().error("{} failed to process due to {}; rolling back", new Object[] {this, t});
+    }
+  }
+
+  private String getId(Document doc) {
+    switch(doc.getString("op")) {
+      case "i":
+      case "d":
+      case "u":
+        return doc.get("o", Document.class).getObjectId("_id").toHexString();
+      case "n":
+      case "c":
+        return Long.toString(doc.getLong("h"));
+      default:
+        return "NA";
+    }
+  }
+}
